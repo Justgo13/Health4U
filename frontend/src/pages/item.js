a9dba24252405726c4176e6144b050a9bd9cd17c
justgo13 <jgao2299@gmail.com>
2021-12-30 19:52:50 -0500

    finishing touches on item page

--- frontend/src/pages/item.js
+++ frontend/src/pages/item.js
@@ -14,49 +14,202 @@ import "../styles/item.css";
 const item = {
   id: "1",
   name: "Black mask",
+  category: "mask",
   description: "Black facial mask",
   image:
     "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80",
   price: 2.12,
+  rating: 2.2,
 };
 
-const relatedItems = [
+const allItems = [
   {
     id: "1",
     name: "Random Item #1",
+    category: "mask",
     image:
       "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80",
   },
   {
     id: "2",
     name: "Random Item #2",
+    category: "mask",
     image:
       "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80",
   },
   {
     id: "3",
     name: "Random Item #3",
+    category: "kit",
     image:
       "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80",
   },
   {
     id: "4",
     name: "Random Item #4",
+    category: "kit",
     image:
       "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80",
   },
 ];
 
+const relatedItems = allItems.filter((i) => i.category === item.category);
+
 const Item = () => {
+  const [bookMarkClicked, setBookMarkClicked] = useState(false);
+
+  const roundHalf = (num) => {
+    return Math.round(num * 2) / 2;
+  };
+
+  // had to hardcoded ratings, could not find a way to dynamically generate them
+  const displayStars = (itemRating) => {
+    itemRating = roundHalf(itemRating); // rounds ratings such as 2.2 to 2.0
+    switch (itemRating) {
+      case 0.5:
+        return (
+          <Fragment>
+            <FontAwesomeIcon className="fa-star-half-alt orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+          </Fragment>
+        );
+
+      case 1:
+        return (
+          <Fragment>
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+          </Fragment>
+        );
+      case 1.5:
+        return (
+          <Fragment>
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star-half-alt orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+          </Fragment>
+        );
+      case 2.0:
+        return (
+          <Fragment>
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+          </Fragment>
+        );
+
+      case 2.5:
+        return (
+          <Fragment>
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star-half-alt orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+          </Fragment>
+        );
+
+      case 3.0:
+        return (
+          <Fragment>
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+          </Fragment>
+        );
+
+      case 3.5:
+        return (
+          <Fragment>
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star-half-alt orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+          </Fragment>
+        );
+
+      case 4.0:
+        return (
+          <Fragment>
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+          </Fragment>
+        );
+      case 4.5:
+        return (
+          <Fragment>
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star-half-alt orange" />
+          </Fragment>
+        );
+      case 5.0:
+        return (
+          <Fragment>
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+            <FontAwesomeIcon className="fa-star orange" />
+          </Fragment>
+        );
+      default:
+        return (
+          <Fragment>
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+            <FontAwesomeIcon baseClassName="far" className="fa-star orange" />
+          </Fragment>
+        );
+    }
+  };
+
+  const bookmarkHandler = (e) => {
+    setBookMarkClicked(!bookMarkClicked);
+  };
+
   return (
     <Fragment>
       <MuiBox className="small-box right-align">
-        <CustomButton variant="text" className="black no-background no-padding">
-          <FontAwesomeIcon
-            baseClassName="far"
-            className="fa-bookmark"
-            fontSize="3rem"
-          />
+        <CustomButton
+          variant="text"
+          className="black no-background no-padding"
+          onClick={bookmarkHandler}
+        >
+          {!bookMarkClicked && (
+            <FontAwesomeIcon
+              baseClassName="far"
+              className="fa-bookmark"
+              fontSize="3rem"
+            />
+          )}
+
+          {bookMarkClicked && (
+            <FontAwesomeIcon
+              className="fa-bookmark"
+              fontSize="3rem"
+            />
+          )}
           <span className="small-plus">
             <FontAwesomeIcon className="fa-plus" />
           </span>
@@ -66,22 +219,30 @@ const Item = () => {
       <MuiBox className="flex-container container">
         <MuiBox className="flex-child">
           <h3>{item.name} </h3>
+
+          <p>{displayStars(item.rating)}</p>
+
           <CardMedia
             className="image"
             component="img"
             image={item.image}
             alt={item.name}
           />
-
-          <MuiBox className="item-desc">
-            <MuiDivider
-              children={<h2 className="red-header">Product Details</h2>}
-            />
-            <p>{item.description}</p>
-          </MuiBox>
         </MuiBox>
+
         <MuiBox className="price-box flex-child">
           <MuiForm />
+        </MuiBox>
+      </MuiBox>
+
+      <MuiBox className="container item-desc no-bottom-padding">
+        <MuiDivider
+          children={<h2 className="red-header">Product Details</h2>}
+        />
+        <MuiBox className="no-bottom-padding">
+          <p>Description - {item.description}</p>
+          <p>Manufacturer - Health4U</p>
+          <p>First available - December 30, 2021</p>
         </MuiBox>
       </MuiBox>
 
