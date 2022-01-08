f908424c0cd27608b3a318f0e3d58e249746591d
justgo13 <jgao2299@gmail.com>
2022-01-08 08:56:12 -0500

    refactor profile page

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -1,22 +1,132 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect, Fragment } from "react";
 
-import BuyerProfile from "./buyer-profile";
-import SellerProfile from "./seller-profile";
+import Navbar from "../components/NavBar/navbar";
+import MuiDivider from "../components/MaterialUI/mui-divider";
+import MuiBox from "../components/MaterialUI/mui-box";
+import MuiForm from "../components/MaterialUI/mui-form";
+import MuiTypography from "../components/MaterialUI/mui-typography";
+import MuiTextField, {
+  VALIDATE_REQUIRE,
+  VALIDATE_EMAIL,
+} from "../components/MaterialUI/Form/mui-textfield";
+import CustomButton from "../components/custom-button";
+
+import { useFormValidation } from "../components/MaterialUI/Form/form-validation";
 import { useAuthCookies } from "../shared/cookies/auth-cookies";
 
 const Profile = () => {
-  const { userID } = useParams();
-  const { getUserInfo } = useAuthCookies();
 
+  const { updateUserInfo, getUserInfo } = useAuthCookies();
   const user = getUserInfo();
 
-  if (user.accountType === "Buyer") {
-    return <BuyerProfile user={user} />;
-  } else if (user.accountType === "Seller") {
-    return <SellerProfile user={user} items={user.items} />;
-  }
-  return <h1>Invalid ShopPage</h1>;
+  const { formValidationState, updateFormValidationState, verifyForm } =
+    useFormValidation(
+      [
+        {
+          name: "name",
+          value: "",
+          isValid: false,
+        },
+        {
+          name: "email",
+          value: "",
+          isValid: false,
+        },
+      ],
+      false
+    );
+
+  const [editFields, setEditFields] = useState(false);
+
+  const editHandler = () => {
+    setEditFields(true);
+  };
+
+  const saveEditHandler = (e) => {
+    e.preventDefault();
+
+    setEditFields(false);
+
+    if (formValidationState.isValid) {
+      const name = formValidationState.inputs.find(
+        (item) => item.name === "name"
+      ).value;
+      const email = formValidationState.inputs.find(
+        (item) => item.name === "email"
+      ).value;
+      updateUserInfo({
+        name,
+        email,
+        accountType: user.accountType,
+      });
+    }
+  };
+
+  useEffect(() => {
+    verifyForm();
+  }, [formValidationState.inputs]);
+
+  return (
+    <Fragment>
+      <Navbar />
+
+      <MuiBox className="container top-bottom-padding">
+        <MuiDivider headerText={`${user.name}'s Profile`} />
+      </MuiBox>
+
+      <MuiBox className="container">
+        <MuiForm
+          formHeader={
+            <MuiTypography className="divider-header top-bottom-padding center">
+              Profile details
+            </MuiTypography>
+          }
+        >
+          <MuiBox className="grey-background container textfield-group">
+            <MuiTextField
+              label="Name"
+              validators={[VALIDATE_REQUIRE]}
+              formInput={formValidationState.inputs.find(
+                (input) => input.name === "name"
+              )}
+              updateFormValidationState={updateFormValidationState}
+              disabled={!editFields}
+              defaultValue={user.name}
+              defaultValid
+            />
+            <MuiTextField
+              label="Email"
+              validators={[VALIDATE_REQUIRE, VALIDATE_EMAIL]}
+              formInput={formValidationState.inputs.find(
+                (input) => input.name === "email"
+              )}
+              updateFormValidationState={updateFormValidationState}
+              disabled={!editFields}
+              defaultValue={user.email}
+              defaultValid
+            />
+
+            {!editFields && (
+              <CustomButton
+                className="big-btn white-inverse top-bottom-margin"
+                onClick={editHandler}
+              >
+                Edit Fields
+              </CustomButton>
+            )}
+            {editFields && (
+              <CustomButton
+                className="big-btn white-inverse top-bottom-margin"
+                onClick={saveEditHandler}
+              >
+                Save Changes
+              </CustomButton>
+            )}
+          </MuiBox>
+        </MuiForm>
+      </MuiBox>
+    </Fragment>
+  );
 };
 
 export default Profile;
